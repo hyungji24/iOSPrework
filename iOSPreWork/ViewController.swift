67a13670a58ebf639f3f300ed90743af3e655403
hyungji24 <75115435+hyungji24@users.noreply.github.com>
2022-01-01 03:07:48 +0900

    Changed colors and edited README.md

--- iOSPreWork/ViewController.swift
+++ iOSPreWork/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     
     @IBAction func TextButtonClicked(_ sender: Any) {
         if inputField.text == "" {
-            TextLabel.text = "Goodbye"
+            TextLabel.text = "Goodbye 👋"
         } else {
             TextLabel.text = inputField.text
             inputField.text = ""
@@ -29,15 +29,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func TextColorButtonClicked(_ sender: Any) {
-        TextLabel.textColor = UIColor.orange
+        if TextLabel.textColor == UIColor.systemIndigo {
+            TextLabel.textColor = UIColor.systemPink
+        } else {
+            TextLabel.textColor = UIColor.systemIndigo
+        }
     }
     
     @IBAction func BackgroundButtonClicked(_ sender: Any) {
-        view.backgroundColor = UIColor.yellow
+        view.backgroundColor = UIColor.systemIndigo
     }
     
     @IBAction func TapBackground(_ sender: UITapGestureRecognizer) {
-        view.backgroundColor = UIColor.white
+        // setting everything back to default
+        view.backgroundColor = #colorLiteral(red: 0.4257946908, green: 0.630717814, blue: 0.7827987075, alpha: 1)
+        TextLabel.text = "Hello from Hyungji!"
+        TextLabel.textColor = UIColor.systemIndigo
+
     }
 
 }
